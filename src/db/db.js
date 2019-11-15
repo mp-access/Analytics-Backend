eae0c3c95f47899590ab6f2bbb4088ed18fa4042
Alexander Hofmann <alexhofmann@gmail.com>
2019-11-15 16:22:31 +0100

    Add db auth on prod

--- src/db/db.js
+++ src/db/db.js
@@ -1,28 +1,25 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const dbUrl = process.env.MONGODB_URL;
+let dbUrl = process.env.MONGODB_URL;
 const dbUsername = process.env.MONGO_DB_USERNAME;
 const dbPassword = process.env.MONGO_DB_PASSWORD;
 
 const initDb = async (isDev) => {
     return new Promise((resolve, reject) => {
+
+        if (!isDev) {
+            const parts = dbUrl.split("://");
+            dbUrl = parts.join(`://${dbUsername}:${dbPassword}@`);
+            console.log("Added authentication to mongodb connection string")
+        }
+
         MongoClient.connect(dbUrl, (err, client) => {
             if (err) {
+                console.log(err);
                 reject(err);
-            }
-
-            if (isDev) {
+            } else {
                 const db = client.db('dev');
                 resolve(db);
-            } else {
-                const db = client.db('access');
-                db.authenticate(dbUsername, dbPassword, (err, result) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(db);
-                    }
-                })
             }
         })
     });
